3dcc487f81dc54815e9e80afcff6bd6adac24448
Santiago Cuello <sjcuello@gmail.com>
2024-04-16 13:15:57 -0300

    Log on App demo

--- packages/MainUI/src/App.tsx
+++ packages/MainUI/src/App.tsx
@@ -1,19 +1,19 @@
 import { useEffect } from 'react';
 import Home from './screens/Home';
-import {columsPageMetadata} from '@workspaceui/etendohookbinder/src/services/sales-order.service.ts';
+import { columsPageMetadata } from '@workspaceui/etendohookbinder/src/services/sales-order.service.ts';
 import { pageMetadata } from '@workspaceui/etendohookbinder/src/api/sales-order.api';
 
 function App() {
   //Example for DEMO: The importance to use the DTOs insted of the raw data
   useEffect(() => {
-    pageMetadata().then((res) => {
-      console.log('pageMetadata:',res);
-     }) 
-    columsPageMetadata().then((res) => {
+    pageMetadata().then(res => {
+      console.log('pageMetadata:', res);
+    });
+    columsPageMetadata().then(res => {
       console.log('columsPageMetadata:', res);
-    })
-  }, [])
-  
+    });
+  }, []);
+
   return <Home />;
 }
 
