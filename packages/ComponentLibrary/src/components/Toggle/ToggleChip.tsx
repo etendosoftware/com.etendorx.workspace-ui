daba6d37a47094248fe9cc81e47d3f6cbbd8fc8e
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-05-22 12:24:47 -0300

    feature EPL-1492: Creating types files for interface

--- packages/ComponentLibrary/src/components/Toggle/ToggleChip.tsx
+++ packages/ComponentLibrary/src/components/Toggle/ToggleChip.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import Switch, { SwitchProps } from '@mui/material/Switch';
+import Switch from '@mui/material/Switch';
 import { styled, Theme } from '@mui/material/styles';
 import switchStyles from './Toggle.styles';
+import { ToggleChipProps } from './types';
 
 const StyledSwitch = styled(Switch)(({ theme }: { theme: Theme }) =>
   switchStyles(theme),
 );
-
-interface ToggleChipProps extends SwitchProps {}
 
 const ToggleChip: React.FC<ToggleChipProps> = props => (
   <StyledSwitch {...props} />
