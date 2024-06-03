a9378f5933299b602629453c8a7894293d5a8350
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-03 15:24:48 -0300

    Feature EPL-1484: Toggle Button first version

---
+++ packages/ComponentLibrary/src/components/ProfileModal/ToggleButton/ToggleButton.styles.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from 'react';
+
+export const buttonStyles: CSSProperties = {
+  fontWeight: 'bold',
+  fontSize: '1rem',
+  padding: '0.5rem 1rem',
+  cursor: 'pointer',
+  borderRadius: '4px',
+};
