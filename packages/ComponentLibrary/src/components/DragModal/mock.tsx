5c63a548ffdf8756c2d6c835fe87526deb915e7e
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-12 11:21:34 -0300

    Feature EPL-1491: Mock Fix

--- packages/ComponentLibrary/src/components/DragModal/mock.tsx
+++ packages/ComponentLibrary/src/components/DragModal/mock.tsx
@@ -1,17 +1,10 @@
 import { Person } from './DragModal.types';
 
 export const people: Person[] = [
-<<<<<<< Updated upstream
-  { id: '1', label: 'Person 1' },
-  { id: '2', label: 'Person 2' },
-  { id: '3', label: 'Person 3' },
-  { id: '4', label: 'Person 4' },
-=======
   { id: '1', label: 'Trabajo', isActive: false },
   { id: '2', label: 'Pedido de venta', isActive: false },
   { id: '3', label: 'Factura de venta', isActive: false },
   { id: '4', label: 'Producto', isActive: false },
   { id: '5', label: 'Factura de tercero pagada', isActive: false },
   { id: '6', label: 'Comprobante de tercero para devolución', isActive: false },
->>>>>>> Stashed changes
 ];
