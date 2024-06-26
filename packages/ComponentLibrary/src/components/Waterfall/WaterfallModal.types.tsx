c7f5e8b133de5877c8627131bbba2dc45a0bc76b
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-26 16:19:11 -0300

    Feature EPL-1490: Adding text as props and make styles cleaner

--- packages/ComponentLibrary/src/components/Waterfall/WaterfallModal.types.tsx
+++ packages/ComponentLibrary/src/components/Waterfall/WaterfallModal.types.tsx
@@ -2,9 +2,15 @@ import { UniqueIdentifier } from '@dnd-kit/core';
 import { Person } from '../DragModal/DragModal.types';
 
 export interface WaterfallModalProps {
+  menuItems: { emoji: string; label: string; key: string }[];
   people: Person[];
+  initialPeople: Person[];
   onBack: () => void;
   onToggleAll: () => void;
   onToggle: (id: UniqueIdentifier) => void;
   setPeople: (people: Person[]) => void;
+  backButtonText?: string;
+  activateAllText?: string;
+  deactivateAllText?: string;
+  customizeText?: string;
 }
