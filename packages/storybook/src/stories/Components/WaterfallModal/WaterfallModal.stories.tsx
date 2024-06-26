2b7ff23572b860f826ea436ec20fafd26febbae1
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-26 16:07:33 -0300

    Feature EPL-1607: [Storybook] - Waterfall items

---
+++ packages/storybook/src/stories/Components/WaterfallModal/WaterfallModal.stories.tsx
@@ -0,0 +1,16 @@
+import WaterfallModal from '../../../../../ComponentLibrary/src/components/Waterfall/WaterfallModal';
+import { WaterfallModalProps } from '../../../../../ComponentLibrary/src/components/Waterfall/WaterfallModal.types';
+import { initialPeople, menuItems } from '../constants';
+
+export default {
+  title: 'Components/WaterfallModal',
+  component: WaterfallModal,
+};
+
+const Template = (args: WaterfallModalProps) => <WaterfallModal {...args} />;
+
+export const DefaultWaterfallModal = Template.bind({});
+DefaultWaterfallModal.args = {
+  menuItems,
+  initialPeople,
+};
