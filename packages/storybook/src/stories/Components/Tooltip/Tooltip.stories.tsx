c11fc72e711b726ba375af4b9e62ac66a235768f
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-27 10:50:28 -0300

    Feature EPL-1611: [Storybook] - Tooltip

---
+++ packages/storybook/src/stories/Components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,10 @@
+import TooltipDemo from '../../../../../ComponentLibrary/src/components/Tooltip';
+
+export default {
+  title: 'Components/Tooltip',
+  component: TooltipDemo,
+};
+
+const Template = () => <TooltipDemo />;
+
+export const Default = Template.bind({});
