8195e6e547943f6088a80cd7cf25cace1730ff0f
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-06-28 09:36:47 -0300

    Feature EPL-1603: [Storybook] Mock traduction

--- packages/storybook/src/stories/Components/ToggleButton/mock.tsx
+++ packages/storybook/src/stories/Components/ToggleButton/mock.tsx
@@ -5,19 +5,19 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { Section } from '../../../../../ComponentLibrary/src/components/ProfileModal/ToggleButton/types';
 
 export const sectionsMock: Section[] = [
-  { id: 'profile', label: 'Perfil', icon: <PersonOutlineIcon /> },
-  { id: 'password', label: 'Contraseña', icon: <LockIcon /> },
+  { id: 'profile', label: 'Profile', icon: <PersonOutlineIcon /> },
+  { id: 'password', label: 'Password', icon: <LockIcon /> },
 ];
 
 export const sectionsMock3: Section[] = [
-  { id: 'profile', label: 'Perfil', icon: <PersonOutlineIcon /> },
-  { id: 'password', label: 'Contraseña', icon: <LockIcon /> },
-  { id: 'home', label: 'Inicio', icon: <HomeIcon /> },
+  { id: 'profile', label: 'Profile', icon: <PersonOutlineIcon /> },
+  { id: 'password', label: 'Password', icon: <LockIcon /> },
+  { id: 'home', label: 'Home', icon: <HomeIcon /> },
 ];
 
 export const sectionsMock4: Section[] = [
-  { id: 'profile', label: 'Perfil', icon: <PersonOutlineIcon /> },
-  { id: 'password', label: 'Contraseña', icon: <LockIcon /> },
-  { id: 'home', label: 'Inicio', icon: <HomeIcon /> },
-  { id: 'settings', label: 'Configuración', icon: <SettingsIcon /> },
+  { id: 'profile', label: 'Profile', icon: <PersonOutlineIcon /> },
+  { id: 'password', label: 'Password', icon: <LockIcon /> },
+  { id: 'home', label: 'Home', icon: <HomeIcon /> },
+  { id: 'settings', label: 'Settings', icon: <SettingsIcon /> },
 ];
