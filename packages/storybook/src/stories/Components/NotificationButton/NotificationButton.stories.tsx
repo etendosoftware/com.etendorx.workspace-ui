cf5b9b15ea80981cc02d90fcf4cf9c6e232532d6
Santiago Alaniz <santiago.alaniz@smfconsulting.es>
2024-07-10 18:57:23 -0300

    Feature EPL-1561: [Navbar] All states of notification icon

--- packages/storybook/src/stories/Components/NotificationButton/NotificationButton.stories.tsx
+++ packages/storybook/src/stories/Components/NotificationButton/NotificationButton.stories.tsx
@@ -1,5 +1,5 @@
+import Grid from '@mui/material/Grid';
 import NotificationButton from '../../../../../ComponentLibrary/src/components/NotificationsButton';
-import { NOTIFICATIONS } from '../notifications.mock';
 
 export default {
   title: 'Components/NotificationButton',
@@ -9,10 +9,40 @@ export default {
   },
 };
 
-const Template = args => <NotificationButton {...args} />;
+// Mock de notificaciones extendido
+const generateMockNotifications = count => {
+  return Array.from({ length: count }, (_, index) => ({
+    id: `${index + 1}`,
+  }));
+};
+
+const Template = args => (
+  <Grid container spacing={6}>
+    <Grid item xs={1}>
+      <NotificationButton
+        {...args}
+        notifications={generateMockNotifications(100)}
+      />
+    </Grid>
+    <Grid item xs={1}>
+      <NotificationButton
+        {...args}
+        notifications={generateMockNotifications(99)}
+      />
+    </Grid>
+    <Grid item xs={1}>
+      <NotificationButton
+        {...args}
+        notifications={generateMockNotifications(7)}
+      />
+    </Grid>
+    <Grid item xs={1}>
+      <NotificationButton {...args} notifications={[]} />
+    </Grid>
+  </Grid>
+);
 
 export const NotificationButtonDefault = Template.bind({});
 NotificationButtonDefault.args = {
-  notifications: NOTIFICATIONS,
   tooltipTitle: 'Notificaciones',
 };
