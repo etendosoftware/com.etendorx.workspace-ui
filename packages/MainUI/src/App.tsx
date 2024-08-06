06c45cd908e6e7d100760c380dea1ef78a208a63
Luciano Rocha <luciano.rocha@etendo.software>
2024-08-06 16:08:13 -0300

    Feature ETP-80: add window id as url parameter

--- packages/MainUI/src/App.tsx
+++ packages/MainUI/src/App.tsx
@@ -1,12 +1,16 @@
 import MetadataProvider from '@workspaceui/etendohookbinder/src/contexts/metadata';
 import Home from './screens/Home';
 import DynamicTable from './screens/DynamicTable';
+import { useState } from 'react';
 
 export default function App() {
+  const [windowId] = useState(
+    new URLSearchParams(location.search).get('windowId') ?? '143',
+  );
   return (
     <MetadataProvider>
       <Home>
-        <DynamicTable windowId="143" />
+        <DynamicTable windowId={windowId} />
       </Home>
     </MetadataProvider>
   );
