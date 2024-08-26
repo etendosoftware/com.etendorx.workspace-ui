25b4bfe761ebd7768d7e032b8d9eac176e1a6035
Luciano Rocha <luciano.rocha@etendo.software>
2024-08-26 11:14:20 -0300

    Feature ETP-244: Fix build

--- packages/MainUI/src/screens/Home/types.ts
+++ packages/MainUI/src/screens/Home/types.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Organization } from '@workspaceui/storybook/stories/Components/Table/types';
+import { MRT_Row } from 'material-react-table';
+
 export interface ContentProps {
   isFormView: boolean;
   selectedItem: Organization | null;
