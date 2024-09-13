fcc5bb3ec6e3d127c49050a4319c511595b00c37
Luciano Rocha <luciano.rocha@etendo.software>
2024-09-13 11:07:33 -0300

    Feature ETP-237: Minor improvements

--- packages/EtendoHookBinder/src/helpers/metadata.ts
+++ packages/EtendoHookBinder/src/helpers/metadata.ts
@@ -1,37 +1,17 @@
 import type { Etendo } from '../api/metadata';
 
 export const parseColumns = (columns?: Etendo.Field[]): Etendo.Column[] => {
-  return (columns ?? []) as unknown as Etendo.Column[];
-
-  console.debug({ columns });
   if (!columns) {
     return [];
   }
 
-  return columns
-    .filter(column => {
-      if (!column.gridProps) {
-        return false;
-      } else {
-        return column.gridProps.showIf !== 'false';
-      }
-    })
-    .sort((columnA, columnB) => {
-      if (columnA.gridProps.sort < columnB.gridProps.sort) {
-        return -1;
-      } else {
-        return 1;
-      }
-    })
-    .map(column => ({
-      header: column.title ?? column.name ?? column.columnName,
-      id: column.name,
-      accessorFn: (v: Record<string, unknown>) => {
-        const identifier = column.name + '$_identifier';
+  return columns.map(column => ({
+    header: column.title ?? column.name ?? column.columnName,
+    id: column.name,
+    accessorFn: (v: Record<string, unknown>) => {
+      const identifier = column.columnName + '$_identifier';
 
-        return typeof v[identifier] !== 'undefined'
-          ? v[identifier]
-          : v[column.name];
-      },
-    }));
+      return v[identifier] ?? v[column.columnName];
+    },
+  }));
 };
