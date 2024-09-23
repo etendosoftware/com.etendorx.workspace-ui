4c119ed2475c4f2ba099593e21343bb195007720
Luciano Rocha <luciano.rocha@etendo.software>
2024-09-22 22:26:55 -0300

    Feature ETP-267: Working on dynamic form view

--- packages/ComponentLibrary/src/components/Drawer/DrawerSection/index.tsx
+++ packages/ComponentLibrary/src/components/Drawer/DrawerSection/index.tsx
@@ -19,8 +19,8 @@ export default function DrawerSection({
   const handleClick = useCallback(() => {
     if (item.children?.length) {
       setExpanded(prev => !prev);
-    } else if (item.window?.id) {
-      onClick(`/window/${item.window.id}`);
+    } else if (item.windowId) {
+      onClick(`/window/${item.windowId}`);
     } else {
       console.error('DrawerSection: unexpected type');
     }
--- packages/MainUI/src/screens/Form/DynamicFormView.tsx
+++ packages/MainUI/src/screens/Form/DynamicFormView.tsx
@@ -1,13 +1,18 @@
-import { useCallback, useMemo } from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useCallback, useEffect, useMemo } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Spinner from '@workspaceui/componentlibrary/src/components/Spinner';
-import { useEntityRecord } from '@workspaceui/etendohookbinder/src/hooks/useEntityRecord';
 import { useMetadataContext } from '@workspaceui/etendohookbinder/src/hooks/useMetadataContext';
 import { Etendo } from '@workspaceui/etendohookbinder/src/api/metadata';
+import { useEntityRecord } from '@workspaceui/etendohookbinder/src/hooks/useEntityRecord';
 
 export default function DynamicFormView() {
   const { recordId = '' } = useParams<{ recordId: string }>();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    console.debug({ recordId });
+  }, [recordId]);
 
   const {
     windowData,
@@ -16,68 +21,71 @@ export default function DynamicFormView() {
     error: windowError,
   } = useMetadataContext();
 
-  const {
-    data,
-    loading: recordLoading,
-    error: recordError,
-  } = useEntityRecord(windowData?.tabs[0].entityName ?? '', recordId);
-
-  const handleSave = useCallback(() => navigate('/'), [navigate]);
-  const handleCancel = useCallback(() => navigate('/'), [navigate]);
-
-  const fields = useMemo(() => {
-    const sections = {} as Record<string, unknown[]>;
-    columnsData[windowData.tabs[0].id].forEach(field => {
-      const group = field.fieldGroup ?? 'default';
-      sections[group] = sections[group] ?? [];
-      sections[group].push(field);
-    });
-
-    return sections;
-  }, [columnsData]);
-
-  if (windowLoading || recordLoading) return <Spinner />;
-  if (windowError || recordError)
-    return <div>Error: {windowError?.message || recordError?.message}</div>;
-
-  console.log(fields);
-
-  return (
-    <>
-      {Object.entries(windowData.tabs[0].fields).map(([key, value]) => {
-        return <MagicField key={key} {...value} />;
-      })}
-    </>
+  const { data } = useEntityRecord(
+    windowData?.tabs[0].entityName ?? '',
+    recordId,
   );
+
+  // const handleSave = useCallback(() => navigate('/'), [navigate]);
+  // const handleCancel = useCallback(() => navigate('/'), [navigate]);
+
+  // const fields = useMemo(() => {
+  //   const sections = {} as Record<string, unknown[]>;
+  //   columnsData[windowData.tabs[0].id].forEach(field => {
+  //     const group = field.fieldGroup ?? 'default';
+  //     sections[group] = sections[group] ?? [];
+  //     sections[group].push(field);
+  //   });
+
+  //   return sections;
+  // }, [columnsData, windowData.tabs]);
+
+  console.debug(data);
+
+  if (windowLoading) {
+    return <Spinner />;
+  } else if (windowError) {
+    return <div>Error: {windowError?.message}</div>;
+  } else {
+    return (
+      <>
+        {Object.entries(windowData.tabs[0].fields).map(([key, value]) => {
+          return <MagicField key={key} {...value} data={data} />;
+        })}
+      </>
+    );
+  }
 }
 
-const TextField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
+const TextField = (props: Etendo.Field & { data?: any }) => {
+  console.log(props.column);
+
+  return (
+    <input
+      type="text"
+      title={props.column.description}
+      placeholder={props.column.name}
+      className="field"
+    />
+    // <div className="field">
+    //   {props.column.name}
+    //   <br />
+    //   {props.column.dBColumnName}
+    //   <br />
+    //   <small>
+    //     {JSON.stringify(props.column, null, 2)}
+    //     <br />
+    //   </small>
+    // </div>
+  );
 };
 
-const DateField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
-
-const DateTimeField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
-
-const ListField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
-
-const TableField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
-
-const TableDirField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
-
-const BooleanField = (props: Etendo.Field) => {
-  return <div>{props.column.reference$_identifier}</div>;
-};
+const DateField = TextField;
+const DateTimeField = TextField;
+const ListField = TextField;
+const TableField = TextField;
+const TableDirField = TextField;
+const BooleanField = TextField;
 
 const Components = {
   '14': TextField,
@@ -89,7 +97,7 @@ const Components = {
   '20': BooleanField,
 };
 
-const MagicField = (props: Etendo.Field) => {
+const MagicField = (props: Etendo.Field & { data?: any }) => {
   const Cmp =
     Components[props.column.reference as keyof typeof Components] ?? TextField;
 
