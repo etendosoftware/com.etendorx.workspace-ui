aa73eb4831bbb1de9efbf96acef0a99f6dd4cff1
Luciano Rocha <luciano.rocha@smfconsulting.es>
2024-10-10 18:53:58 -0300

    Feature ETP-452: Added sanity checker.

--- packages/MainUI/src/components/SanityChecker/index.tsx
+++ packages/MainUI/src/components/SanityChecker/index.tsx
@@ -1,41 +1,44 @@
-import { API_LOGIN_URL, MAX_ATTEMPTS } from '@workspaceui/etendohookbinder/api/constants';
+import { API_METADATA_URL, MAX_ATTEMPTS } from '@workspaceui/etendohookbinder/api/constants';
 import { useEffect, useRef, useState } from 'react';
 import { logger } from '../../utils/logger';
 import Spinner from '@workspaceui/componentlibrary/components/Spinner';
-import { Box } from '@mui/material';
 
 export default function SanityChecker(props: React.PropsWithChildren) {
   const [connected, setConnected] = useState(false);
+  const checker = useRef(NaN);
   const attempts = useRef(0);
 
   useEffect(() => {
-    const f = async () => {
+    const healthCheck = async () => {
       try {
         if (attempts.current < MAX_ATTEMPTS) {
           attempts.current = attempts.current + 1;
-          const response = await fetch(API_LOGIN_URL);
+          const response = await fetch(API_METADATA_URL, {
+            method: 'OPTIONS',
+          });
 
           if (response.ok) {
             attempts.current = 0;
             setConnected(true);
+            clearInterval(checker.current);
           }
         }
       } catch (e) {
         logger.warn('Could not connect to the API after ' + attempts.current + ' attempts');
-        setTimeout(f, 5000);
       }
     };
 
-    setTimeout(f);
+    healthCheck();
+    checker.current = setInterval(healthCheck, 2000);
+
+    return () => {
+      clearInterval(checker.current);
+    };
   }, []);
 
   if (connected) {
     return <>{props.children}</>;
-  } else {
-    return (
-      <Box flex={1}>
-        <Spinner size="2rem" />
-      </Box>
-    );
   }
+
+  return <Spinner />;
 }
