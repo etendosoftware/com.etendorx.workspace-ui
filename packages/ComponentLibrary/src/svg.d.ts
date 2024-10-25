70ba052856a4e16d1e68dda4b5fc848e073af437
Luciano Rocha <luciano.rocha@etendo.software>
2024-10-25 17:20:29 -0300

    Feature ETP-593: Fix build

--- packages/ComponentLibrary/src/svg.d.ts
+++ packages/ComponentLibrary/src/svg.d.ts
@@ -1,6 +1,6 @@
 declare module '*.svg' {
   import { FC, SVGProps } from 'react';
-  const content: FC<SVGProps<SVGElement>>;
+  const content: FC<SVGProps<SVGSVGElement>>;
   export default content;
 }
 
