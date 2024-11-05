c931c5d1b8b63769fbab3e87a6b7d5efd119d6d8
Luciano Rocha <luciano.rocha@etendo.software>
2024-11-05 14:07:01 -0300

    Feature ETP-695: Remove duplicated theme provider

--- packages/MainUI/app/App.tsx
+++ packages/MainUI/app/App.tsx
@@ -6,26 +6,21 @@ import { LanguageProvider } from '../contexts/languageProvider';
 import MetadataProvider from '../contexts/metadata';
 import { RecordProvider } from '../contexts/record';
 import UserProvider from '../contexts/user';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { theme } from '@workspaceui/componentlibrary/src/theme';
 
 const queryClient = new QueryClient();
 
 export default function App({ children }: React.PropsWithChildren) {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <SanityChecker>
-        <QueryClientProvider client={queryClient}>
-          <LanguageProvider>
-            <UserProvider>
-              <MetadataProvider>
-                <RecordProvider>{children}</RecordProvider>
-              </MetadataProvider>
-            </UserProvider>
-          </LanguageProvider>
-        </QueryClientProvider>
-      </SanityChecker>
-    </ThemeProvider>
+    <SanityChecker>
+      <QueryClientProvider client={queryClient}>
+        <LanguageProvider>
+          <UserProvider>
+            <MetadataProvider>
+              <RecordProvider>{children}</RecordProvider>
+            </MetadataProvider>
+          </UserProvider>
+        </LanguageProvider>
+      </QueryClientProvider>
+    </SanityChecker>
   );
 }
