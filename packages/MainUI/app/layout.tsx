e99694a624a0a9fdefce8fca435891aad1409dd1
Santiago Alaniz <santiago.alaniz@etendo.software>
2024-11-07 10:06:44 -0300

    Feature ETP-714: Added the view for static formview

--- packages/MainUI/app/layout.tsx
+++ packages/MainUI/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
+      <head>
+        <meta charSet="utf-8" />
+      </head>
       <body>
         <ThemeRegistry>
           <App>{children}</App>
