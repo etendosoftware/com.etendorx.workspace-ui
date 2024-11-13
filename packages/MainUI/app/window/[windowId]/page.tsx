592b53d5b9969e934e6fbaf015e74c07ca0ca032
Santiago Alaniz <santiago.alaniz@etendo.software>
2024-11-13 08:28:02 -0300

    Feature ETP-713: Dynamic view fix

--- packages/MainUI/app/window/[windowId]/page.tsx
+++ packages/MainUI/app/window/[windowId]/page.tsx
@@ -1,22 +1,7 @@
 'use client';
 
-import { Tab } from '@workspaceui/etendohookbinder/src/api/types';
-import Spinner from '@workspaceui/componentlibrary/src/components/Spinner';
-import { useMetadataContext } from '../../../hooks/useMetadataContext';
-import Tabs from '../../../screens/Table/Tabs';
+import DynamicTableScreen from '../../../screens/Table/Dynamic';
 
-function Level(value: Tab[]) {
-  return <Tabs key={value[0].id} tabs={value} />;
-}
-
-export default function DynamicTableScreen() {
-  const { loading, error, windowData, groupedTabs } = useMetadataContext();
-
-  if (loading) {
-    return <Spinner />;
-  } else if (error || !windowData) {
-    return <div>{error?.message ?? 'Something went wrong'}</div>;
-  } else {
-    return <div>{groupedTabs.map(tabs => Level(tabs))}</div>;
-  }
+export default function TabPage() {
+  return <DynamicTableScreen />;
 }
