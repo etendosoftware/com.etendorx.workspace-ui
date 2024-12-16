d123ccf40970d62c87952a4294c89b3eb3d24514
Santiago Alaniz <santiago.alaniz@etendo.software>
2024-12-16 10:34:45 -0300

    Feature ETP-871: Fix build and minor issues

--- packages/MainUI/app/report/[reportId]/page.tsx
+++ packages/MainUI/app/report/[reportId]/page.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import { Box } from '@mui/material';
+import SalesOrderReport from '../../../components/ad_reports/SalesOrderReport';
+
 export default function Page() {
-  return <h1>REPORT WINDOW</h1>;
+  return (
+    <>
+      <Box m={2}>
+        <SalesOrderReport />
+      </Box>
+    </>
+  );
 }
