6759dde3fa37ae01478749f3e6a3c0e88fa6941f
Luciano Rocha <luciano.rocha@etendo.software>
2025-01-18 19:42:55 -0300

    Feature ETP-939: Fix tsconfig and language fallback

--- packages/MainUI/hooks/useTranslation.ts
+++ packages/MainUI/hooks/useTranslation.ts
@@ -1,5 +1,5 @@
 import { useLanguage } from './useLanguage';
-import { TranslateFunction, Translations } from './types';
+import { TranslateFunction } from './types';
 import { useCallback, useMemo } from 'react';
 import translations from '@workspaceui/componentlibrary/src/locales';
 
@@ -7,10 +7,10 @@ export const useTranslation = (defaultLanguage = 'en') => {
   const languageContext = useLanguage();
   const language = languageContext?.language || defaultLanguage;
 
-  const t: TranslateFunction = useCallback(
+  const t = useCallback<TranslateFunction>(
     key => {
       const keys = key.split('.');
-      let value: unknown = (translations as Translations)[language];
+      let value: unknown = translations[language ?? 'en_US'];
 
       for (const k of keys) {
         if (typeof value !== 'object' || value === null || !(k in value)) {
