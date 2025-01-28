79c25499b4ccd5388894efdc726f8de456eb9e5c
Luciano Rocha <luciano.rocha@etendo.software>
2025-01-28 01:32:28 -0300

    Feature ETP-1053: Fixed Dockerfile for MainUI.

--- packages/MainUI/build.sh
+++ packages/MainUI/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf .next
+rm -rf ../../dist
 NEXT_TELEMETRY_DISABLED=1 pnpm build
 cp -R public .next/standalone/packages/MainUI/.next
 cp -R .next/static .next/standalone/packages/MainUI/.next
--- packages/MainUI/next.config.ts
+++ packages/MainUI/next.config.ts
@@ -3,18 +3,18 @@ import type { NextConfig } from 'next';
 const DEBUG_MODE = process.env.DEBUG_MODE === "true";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
-  modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
-    },
-    '@mui/icons-material': {
-      transform: '@mui/icons-material/{{member}}',
-    },
-  },
-  reactStrictMode: true,
-  cleanDistDir: true,
-  bundlePagesRouterDependencies: true,
+  // transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+  // modularizeImports: {
+  //   '@mui/material': {
+  //     transform: '@mui/material/{{member}}',
+  //   },
+  //   '@mui/icons-material': {
+  //     transform: '@mui/icons-material/{{member}}',
+  //   },
+  // },
+  // reactStrictMode: true,
+  // cleanDistDir: true,
+  // bundlePagesRouterDependencies: true,
   output: 'standalone',
   compress: !DEBUG_MODE,
   webpack(config) {
