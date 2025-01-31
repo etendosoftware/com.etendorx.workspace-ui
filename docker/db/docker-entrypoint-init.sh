db6f2d920b8862b93170deae4c4e7cf7b77dd308
Luciano Rocha <luciano.rocha@etendo.software>
2025-01-31 20:03:24 -0300

    Feature ETP-1115: Separate docker-compose files

--- docker/db/docker-entrypoint-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ "$FORCE_INIT_DB" = "true" ]; then
-  echo "Forcing PostgreSQL reinitialization..."
-  rm -rf /var/lib/postgresql/data/*
-fi
-
-exec docker-entrypoint.sh postgres
