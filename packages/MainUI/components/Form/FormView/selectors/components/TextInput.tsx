ea0e1de61113707e584637373e006e6b3a78c27b
Luciano Rocha <luciano.rocha@etendo.software>
2025-02-24 23:44:32 -0300

    Feature ETP-1234: Fixed input styles.

--- packages/MainUI/components/Form/FormView/selectors/components/TextInput.tsx
+++ packages/MainUI/components/Form/FormView/selectors/components/TextInput.tsx
@@ -1,7 +1,7 @@
 export const TextInput = (props: React.HTMLProps<HTMLInputElement>) => {
   return (
     <input
-      className="w-full bg-white rounded-2xl border border-gray-300 p-3 text-sm shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-300 outline-none transition"
+      className="w-full h-12 bg-white rounded-2xl border border-gray-300 p-3 text-sm shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-300 focus:outline-none transition"
       {...props}
     />
   );
