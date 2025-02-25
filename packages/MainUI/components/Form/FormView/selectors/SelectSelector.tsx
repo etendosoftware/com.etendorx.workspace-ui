49419a6035c377233f6a47e326cc33fac311f4d8
Luciano Rocha <luciano.rocha@etendo.software>
2025-02-24 23:35:28 -0300

    Feature ETP-1234: Add select selector.

--- packages/MainUI/components/Form/FormView/selectors/SelectSelector.tsx
+++ packages/MainUI/components/Form/FormView/selectors/SelectSelector.tsx
@@ -2,23 +2,28 @@ import { Field } from '@workspaceui/etendohookbinder/src/api/types';
 import { useFormContext } from 'react-hook-form';
 import Select, { SelectProps } from './components/Select';
 import { useMemo } from 'react';
-import { useDatasource } from '@workspaceui/etendohookbinder/src/hooks/useDatasource';
+import { useComboSelect } from '@workspaceui/etendohookbinder/src/hooks/useComboSelect';
 
 export const SelectSelector = ({ field }: { field: Field }) => {
   const { register } = useFormContext();
-  const { records } = useDatasource(field.referencedEntity);
+  const { records } = useComboSelect(field);
+  const idKey = (field.selector?.valueField ?? '') as string;
+  const identifierKey = (field.selector?.displayField ?? '') as string;
+
   const options = useMemo<SelectProps['options']>(() => {
     const result: SelectProps['options'] = [];
 
     records.forEach(record => {
-      result.push({
-        id: record.id as string,
-        label: record._identifier as string,
-      });
+      const option = {
+        id: record[idKey] as string,
+        label: record[identifierKey] as string,
+      };
+
+      result.push(option);
     });
 
     return result;
-  }, [records]);
+  }, [idKey, identifierKey, records]);
 
   return <Select {...register(field.hqlName)} name={field.hqlName} options={options} />;
 };
