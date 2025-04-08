10ff5ebded0804731b436ab3a1b34601a296e39f
Luciano Rocha <luciano.rocha@etendo.software>
2025-04-07 22:05:20 -0300

    Feature ETP-1414: Fix performance issues

--- packages/MainUI/next.config.ts
+++ packages/MainUI/next.config.ts
@@ -3,19 +3,28 @@ import { NextConfig } from 'next';
 const DEBUG_MODE = process.env.DEBUG_MODE === 'true' || process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: DEBUG_MODE,
-  cleanDistDir: false,
+  transpilePackages: ['@mui/material', '@mui/system', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
+  reactStrictMode: true,
+  cleanDistDir: true,
+  bundlePagesRouterDependencies: true,
   output: 'standalone',
-  excludeDefaultMomentLocales: true,
   compiler: {
     removeConsole: !DEBUG_MODE,
   },
   compress: !DEBUG_MODE,
   webpack(config) {
-    // config.optimization = {
-    //   ...config.optimization,
-    //   minimize: !DEBUG_MODE,
-    // };
+    config.optimization = {
+      ...config.optimization,
+      minimize: !DEBUG_MODE,
+    };
 
     const fileLoaderRule = config.module.rules.find(
       (rule: { test: { toString: () => string | string[] } }) => rule.test && rule.test.toString().includes('svg'),
@@ -48,18 +57,18 @@ const nextConfig: NextConfig = {
       },
     );
 
-    // if (config.optimization?.minimizer) {
-    //   config.optimization.minimizer.forEach(
-    //     (plugin: { options?: { terserOptions?: { compress?: { [key: string]: unknown } } } }) => {
-    //       if (plugin.options && plugin.options.terserOptions) {
-    //         plugin.options.terserOptions.compress = {
-    //           ...plugin.options.terserOptions.compress,
-    //           drop_console: !DEBUG_MODE,
-    //         };
-    //       }
-    //     },
-    //   );
-    // }
+    if (config.optimization?.minimizer) {
+      config.optimization.minimizer.forEach(
+        (plugin: { options?: { terserOptions?: { compress?: { [key: string]: unknown } } } }) => {
+          if (plugin.options && plugin.options.terserOptions) {
+            plugin.options.terserOptions.compress = {
+              ...plugin.options.terserOptions.compress,
+              drop_console: !DEBUG_MODE,
+            };
+          }
+        },
+      );
+    }
 
     return config;
   },
