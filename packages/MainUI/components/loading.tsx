c3d2385ab2f2a8f6d4be7767123acd969ef8fd04
Luciano Rocha <luciano.rocha@etendo.software>
2025-04-17 22:50:42 -0300

    Feature ETP-1500: Fix login.

--- packages/MainUI/components/loading.tsx
+++ packages/MainUI/components/loading.tsx
@@ -1,12 +1,9 @@
 import { CircularProgress } from '@mui/material';
-import { DEFAULT_LANGUAGE, Language } from '@workspaceui/componentlibrary/src/locales';
-import { t } from '@/utils/language';
 
-export default function Loading({ language }: { language?: Language }) {
+export default function Loading() {
   return (
     <div className="h-full mx-auto flex flex-col items-center justify-center">
       <CircularProgress />
-      <span>{t(language ?? DEFAULT_LANGUAGE, 'common.loading')}</span>
     </div>
   );
 }
