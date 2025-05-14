2432d508ce4c05a253cac9b19a1bbe96f1b22b9f
Luciano Rocha <luciano.rocha@etendo.software>
2025-05-13 23:28:56 -0300

    Feature ETP-1581: Improve tabs switch

--- packages/MainUI/components/window/container.tsx
+++ packages/MainUI/components/window/container.tsx
@@ -1,4 +1,4 @@
-const BASE_STYLES = 'flex flex-1 flex-col bg-baseline-20 p-2 overflow-hidden min-h-0';
+const BASE_STYLES = 'flex flex-1 flex-col bg-baseline-20 p-2 gap-2 overflow-hidden min-h-0';
 
 export default function Container({ className, ...props }: React.PropsWithChildren<React.HTMLProps<HTMLDivElement>>) {
   return <div {...props} className={`${BASE_STYLES} ${className}`} />;
