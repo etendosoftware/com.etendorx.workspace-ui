828ade424fdaa88665493e2b832ed8195f2e4151
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-06-06 16:21:36 -0300

    Feature ETP-1708: Added new button to close the record

--- packages/MainUI/components/Form/FormView/StatusBar.tsx
+++ packages/MainUI/components/Form/FormView/StatusBar.tsx
@@ -1,12 +1,30 @@
 import type { Field } from "@workspaceui/etendohookbinder/src/api/types";
 import StatusBarField from "./StatusBarField";
+import { IconButton } from "@workspaceui/componentlibrary/src/components";
+import CloseIcon from "@workspaceui/componentlibrary/src/assets/icons/x.svg";
+import { useTranslation } from "@/hooks/useTranslation";
 
 export default function StatusBar({ fields }: { fields: Record<string, Field> }) {
+  const { t } = useTranslation();
+
+  const handleCloseRecord = () => {
+    console.log("close record");
+  };
+
   return (
-    <div className="flex gap-4 bg-gray-100/50 shadow text-sm px-4 py-3 rounded-xl">
-      {Object.entries(fields).map(([key, field]) => (
-        <StatusBarField key={key} field={field} />
-      ))}
+    <div className="h-min flex gap-4 items-center justify-between bg-gray-100/50 shadow text-sm px-4 py-3 rounded-xl">
+      <div>
+        {Object.entries(fields).map(([key, field]) => (
+          <StatusBarField key={key} field={field} />
+        ))}
+      </div>
+      <IconButton
+        onClick={handleCloseRecord}
+        className="w-8 h-8"
+        tooltip={t("forms.statusBar.closeRecord")}
+        disabled={false}>
+        <CloseIcon />
+      </IconButton>
     </div>
   );
 }
