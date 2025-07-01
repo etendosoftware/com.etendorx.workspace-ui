86f68316bf92354dbff8f3dbf35e8c7cc73fd814
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-07-01 16:46:27 -0300

    Feature ETP-1836: Added scroll feature on windows tabs

--- packages/MainUI/components/NavigationTabs/WindowTabs.tsx
+++ packages/MainUI/components/NavigationTabs/WindowTabs.tsx
@@ -4,16 +4,30 @@ import { useMultiWindowURL } from "@/hooks/navigation/useMultiWindowURL";
 import IconButton from "@workspaceui/componentlibrary/src/components/IconButton";
 import HomeIcon from "@workspaceui/componentlibrary/src/assets/icons/home.svg";
 import WindowTab from "@/components/NavigationTabs/WindowTab";
+import { useEffect, useRef, useState } from "react";
 
 export default function WindowTabs() {
   const { windows, setActiveWindow, closeWindow, isHomeRoute, navigateToHome } = useMultiWindowURL();
   const { getWindowTitle } = useMetadataContext();
+  const windowContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = windowContainerRef.current;
+    if (container) {
+      const hasHorizontalScroll = container.scrollWidth > container.clientWidth;
+      
+      if (hasHorizontalScroll) {
+      container.scrollTo({
+        left: container.scrollWidth,
+        behavior: 'smooth'
+      });
+    }
+  }
+  }, [windows]);
 
   const handleGoHome = () => {
     navigateToHome();
   };
-
-  const sortedWindows = windows;
 
   return (
     <div className={"flex items-center bg-(--color-transparent-neutral-5) rounded-full p-0 gap-1 mx-1"}>
@@ -22,9 +36,12 @@ export default function WindowTabs() {
           <HomeIcon />
         </IconButton>
       </div>
-      <div className="flex">
-        {sortedWindows.map((window) => {
-          const title = window.title || getWindowTitle?.(window.windowId) || `Window ${window.windowId}`;
+      <div
+        ref={windowContainerRef}
+        className={`w-full flex overflow-x-auto overflow-y-hidden scroll-smooth no-scrollbar`}
+        >
+        {windows.map((window) => {
+          const title = window.title || getWindowTitle?.(window.windowId);
           const isActive = window.isActive;
           const canClose = windows.length > 1;
           return (
