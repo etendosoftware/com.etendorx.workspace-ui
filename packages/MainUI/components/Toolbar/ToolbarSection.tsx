53d43ff3af5fe6b4c0c062146fce75f3795a8072
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-07-04 15:46:43 -0300

    Feature ETP-1840: Added default values to avoid problems

--- packages/MainUI/components/Toolbar/ToolbarSection.tsx
+++ packages/MainUI/components/Toolbar/ToolbarSection.tsx
@@ -1,9 +1,9 @@
 import type { ToolbarSectionConfig } from "./types";
 import IconButton from "@workspaceui/componentlibrary/src/components/IconButton";
 
-const ToolbarSection: React.FC<ToolbarSectionConfig> = ({ buttons, style, className, processButton }) => {
+const ToolbarSection: React.FC<ToolbarSectionConfig> = ({ buttons, style = {}, className = "", processButton }) => {
   return (
-    <div style={{ ...style }} className={className}>
+    <div style={style} className={className}>
       {buttons.map(({ key, icon, iconText, tooltip, onClick, disabled, className }) => (
         <IconButton
           key={key}
