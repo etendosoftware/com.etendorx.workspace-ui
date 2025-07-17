687c6ca8db4d5b4e7358750adb97095606e5cdd1
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-07-17 09:44:20 -0300

    Feature ETP-1836: Added scroll and adjust styles on tabs menu

--- packages/MainUI/components/NavigationTabs/MenuTabs/index.tsx
+++ packages/MainUI/components/NavigationTabs/MenuTabs/index.tsx
@@ -10,30 +10,22 @@ interface MenuTabsProps {
   onSelect: (windowId: string) => void;
 }
 
-export default function MenuTabs({
-  anchorEl,
-  onClose,
-  onSelect,
-}: MenuTabsProps) {
+export default function MenuTabs({ anchorEl, onClose, onSelect }: MenuTabsProps) {
   const { windows } = useMultiWindowURL();
   const { getWindowTitle } = useMetadataContext();
 
   return (
     <Menu anchorEl={anchorEl} onClose={onClose}>
-      <div className="w-full h-full bg-white rounded-xl flex flex-col gap-2 p-2">
+      <div
+        className="w-full h-full max-h-screen bg-[var(--color-baseline-0)] border border-[var(--color-transparent-neutral-10)] rounded-xl flex flex-col gap-2 p-2 overflow-y-auto"
+        style={{
+          boxShadow: "0px 4px 10px var(--color-transparent-neutral-10)",
+        }}>
         {windows.map((window) => {
           const windowId = window.windowId;
           const title = window.title || getWindowTitle?.(window.windowId);
           const isActive = window.isActive;
-          return (
-            <MenuItem
-              key={windowId}
-              windowId={windowId}
-              title={title}
-              isActive={isActive}
-              onSelect={onSelect}
-            />
-          );
+          return <MenuItem key={windowId} windowId={windowId} title={title} isActive={isActive} onSelect={onSelect} />;
         })}
       </div>
     </Menu>
