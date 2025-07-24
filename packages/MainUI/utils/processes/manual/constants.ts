52e3a592bc355858f40b61ee5330a14b5813a7ca
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-07-24 15:41:25 -0300

    Feature ETP-2032: Removed magic strings on params

--- packages/MainUI/utils/processes/manual/constants.ts
+++ packages/MainUI/utils/processes/manual/constants.ts
@@ -14,3 +14,26 @@ export const DEFAULT_AD_CLIENT_ID_KEYS = [
   "client",
 ];
 export const DEFAULT_AD_ORG_ID_KEYS = ["adOrgId", "AD_Org_ID", "aD_Org_ID", "adorgid", "AdOrgId", "organization"];
+
+export const REQUIRED_PARAMS_KEYS = {
+  isPopUpCall: "IsPopUpCall",
+  command: "Command",
+  inpcOrderId: "inpcOrderId",
+  inpKey: "inpKey",
+  inpwindowId: "inpwindowId",
+  inpTabId: "inpTabId",
+  inpadClientId: "inpadClientId",
+  inpadOrgId: "inpadOrgId",
+  inpkeyColumnId: "inpkeyColumnId",
+  keyColumnName: "keyColumnName",
+  inpdocstatus: "inpdocstatus",
+  inpprocessing: "inpprocessing",
+  inpdocaction: "inpdocaction",
+  token: "token",
+};
+
+export const DEFAULT_REQUIRED_PARAMS_KEYS = {
+  isPopUpCall: "1",
+  inpdocStatus: "CO",
+  inpodcStatusPosted: "P",
+};
