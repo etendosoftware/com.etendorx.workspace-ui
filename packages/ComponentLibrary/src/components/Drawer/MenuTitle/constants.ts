905f2eca887ef9cf0a2205d1a41ff54ab05b5024
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-08-01 13:29:44 -0300

    Feature ETP-2171: Add license to all js-jsx-ts-tsx source files

--- packages/ComponentLibrary/src/components/Drawer/MenuTitle/constants.ts
+++ packages/ComponentLibrary/src/components/Drawer/MenuTitle/constants.ts
@@ -1,3 +1,20 @@
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Etendo License
+ * (the "License"), you may not use this file except in compliance with
+ * the License.
+ * You may obtain a copy of the License at  
+ * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ * All portions are Copyright © 2021–2025 FUTIT SERVICES, S.L
+ * All Rights Reserved.
+ * Contributor(s): Futit Services S.L.
+ *************************************************************************
+ */
+
 export const REPORT_B64 =
   "PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xOCA5QzE4LjU1MjMgOSAxOSA5LjQ0NzcyIDE5IDEwVjIwQzE5IDIwLjU1MjMgMTguNTUyMyAyMSAxOCAyMUMxNy40NDc3IDIxIDE3IDIwLjU1MjMgMTcgMjBWMTBDMTcgOS40NDc3MiAxNy40NDc3IDkgMTggOVoiIGZpbGw9IiMwMDAzMEQiLz4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMiAzQzEyLjU1MjMgMyAxMyAzLjQ0NzcyIDEzIDRWMjBDMTMgMjAuNTUyMyAxMi41NTIzIDIxIDEyIDIxQzExLjQ0NzcgMjEgMTEgMjAuNTUyMyAxMSAyMFY0QzExIDMuNDQ3NzIgMTEuNDQ3NyAzIDEyIDNaIiBmaWxsPSIjMDAwMzBEIi8+CjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCIgY2xpcC1ydWxlPSJldmVub2RkIiBkPSJNNiAxM0M2LjU1MjI4IDEzIDcgMTMuNDQ3NyA3IDE0VjIwQzcgMjAuNTUyMyA2LjU1MjI4IDIxIDYgMjFDNS40NDc3MiAyMSA1IDIwLjU1MjMgNSAyMFYxNEM1IDEzLjQ0NzcgNS40NDc3MiAxMyA2IDEzWiIgZmlsbD0iIzAwMDMwRCIvPgo8L3N2Zz4K";
 
