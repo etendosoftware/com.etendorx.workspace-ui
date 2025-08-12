849cfcc66d7bee1cd1a7a66e37bf94aa4ffd6f3b
leandro.allemandi <leandro.allemandi@smfconsulting.es>
2025-08-12 00:35:09 -0300

    Feature ETP-2219: Fix app

--- packages/MainUI/app/layout.tsx
+++ packages/MainUI/app/layout.tsx
@@ -54,10 +54,13 @@ export default function RootLayout({
    `;
 
   return (
-    <html lang="en" className={inter.variable} suppressHydrationWarning={true}>
+    <html lang="en" className={inter.variable}>
       <head>
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* biome-ignore lint/security/noDangerouslySetInnerHtml: 
+         Apply the density class as early as possible to avoid layout shift. 
+         This script is 100% safe because it doesn't evaluate user input. */}
         <script dangerouslySetInnerHTML={{ __html: setInitialDensityScript }} />
       </head>
       <body>
