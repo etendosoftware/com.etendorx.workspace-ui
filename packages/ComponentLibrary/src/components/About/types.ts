7b6f15773b2cdc8e335fbaa35caeabbdf9c7cdc2
Luciano Palacio <luciano.palacio@smfconsulting.es>
2025-09-19 12:49:37 -0300

    Feature ETP-2312: Add basic modal to show about modal

---
+++ packages/ComponentLibrary/src/components/About/types.ts
@@ -0,0 +1,11 @@
+export interface AboutButtonProps {
+  onClick: () => void;
+  tooltip?: string;
+  disabled?: boolean;
+  iconButtonClassName?: string;
+}
+
+export interface AboutModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
