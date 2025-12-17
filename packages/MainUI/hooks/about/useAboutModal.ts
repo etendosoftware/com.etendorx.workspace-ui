ea0e7fff5d2365de9862cf3f00aa3e002c1378c0
Santiago Alaniz <santiago.alaniz@etendo.software>
2025-12-17 15:42:35 -0300

    Feature ETP-2865: Fix sonar

--- packages/MainUI/hooks/about/useAboutModal.ts
+++ packages/MainUI/hooks/about/useAboutModal.ts
@@ -1,13 +1,9 @@
 import { useUserContext } from "../useUserContext";
-import { useRuntimeConfig } from "../../contexts/RuntimeConfigContext";
 
 const ABOUT_URL_ENDPOINT = "/ad_forms/about.html?IsPopUpCall=1";
 
 export function useAboutModal() {
   const { token } = useUserContext();
-  const { config } = useRuntimeConfig();
-
-  const publicHost = config?.etendoClassicHost || "";
 
   // Point to Next.js API proxy to handle authentication/session validation
   const aboutUrl = `/api/erp${ABOUT_URL_ENDPOINT}&token=${token}`;
