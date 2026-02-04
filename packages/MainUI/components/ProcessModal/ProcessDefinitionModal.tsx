fae172283645919ef9be9b9cee3300c2d888df39
Luciano Palacio <luciano.palacio@smfconsulting.es>
2026-02-04 14:38:19 -0300

    Hotfix ETP-3309: Add context attr to datasource request on process definition

--- packages/MainUI/components/ProcessModal/ProcessDefinitionModal.tsx
+++ packages/MainUI/components/ProcessModal/ProcessDefinitionModal.tsx
@@ -1630,6 +1630,7 @@ function ProcessDefinitionModalContent({ onClose, button, open, onSuccess, type 
             processConfigError={processConfigError}
             recordValues={recordValues}
             currentValues={formValues}
+            originTab={tab}
             data-testid="WindowReferenceGrid__761503"
           />
         );
