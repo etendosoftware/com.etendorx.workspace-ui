3543b4579158f73e3dd82e00078d796056f90274
Luciano Palacio <luciano.palacio@smfconsulting.es>
2026-03-04 15:33:00 -0300

    Hotfix ETP-3309: Apply data check id

--- packages/MainUI/components/ProcessModal/utils/tests/ProcessParameterTestUtils.tsx
+++ packages/MainUI/components/ProcessModal/utils/tests/ProcessParameterTestUtils.tsx
@@ -19,13 +19,21 @@ export const TestWrapper = ({
   defaultValues = { dummy: "value" },
 }: { children: React.ReactNode; defaultValues?: any }) => {
   const form = useForm({ defaultValues });
-  return <FormProvider {...form}>{children}</FormProvider>;
+  return (
+    <FormProvider {...form} data-testid="FormProvider__b68e38">
+      {children}
+    </FormProvider>
+  );
 };
 
 export const setupParameterTest = (parameter: any, logicFields?: any) => {
   render(
-    <TestWrapper>
-      <ProcessParameterSelector parameter={parameter} logicFields={logicFields} />
+    <TestWrapper data-testid="TestWrapper__b68e38">
+      <ProcessParameterSelector
+        parameter={parameter}
+        logicFields={logicFields}
+        data-testid="ProcessParameterSelector__b68e38"
+      />
     </TestWrapper>
   );
   return screen;
